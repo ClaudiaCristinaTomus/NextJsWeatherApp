eca52f3e7014fa7fb24592d620cbf6810c8f8389
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-06-10 15:05:00 +0300

    2nd commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -16,7 +16,28 @@ const navLinks : NavLink[] = [
 
 const Navbar: React.FC = () => {
   return (
-    <nav className='bg-gray-800 py-4'
+    <>
+    <div className='bg-gray-800 py-4'>
+      <div className='container mx-auto px-4'>
+      <div className='flex items-center justify-between'>
+        <h1 className='text-white text-2xl font-semibold'>
+          Weather<span className='text-yellow-500'></span>
+        </h1>
+        <ul className='flex items-center space-x-4 md:space-x-8'>
+          {navLinks.map((link,index)=>(
+            <li key={index}>
+              <Link
+              href={link.url}
+              className='text-white hover:text-yellow-500'
+              {link.label}>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
